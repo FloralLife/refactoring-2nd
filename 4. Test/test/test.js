6570f154ba02c2e5f659233864574fecca37a7a7
FloralLife <ocard9611@naver.com>
2022-03-14 01:28:10 +0900

    사용자가 개발자의 의도대로 사용한 경우 테스트

--- 4. Test/test/test.js
+++ 4. Test/test/test.js
@@ -1,10 +1,26 @@
+import { beforeEach, expect, test } from '@jest/globals';
+import { describe } from 'yargs';
 import { sampleProvinceData } from '../data.js';
-import Producer from '../Producer.js';
 import Province from '../Province.js';
 
 describe('province', function () {
-  it('shortfall', function () {
-    const asia = new Province(sampleProvinceData());
+  let asia;
+
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
+  test('shortfall', function () {
     expect(asia.shortfall).toEqual(5);
+  });
+
+  test('profit', function () {
+    expect(asia.profit).toEqual(230);
+  });
+
+  test('change production', function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6);
+    expect(asia.profit).toEqual(292);
   });
 });
